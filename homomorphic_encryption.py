8acd682a60f1cc5348810d51cccdd2c3ac54d059
Vitalik Buterin <v@buterin.com>
2020-04-14 10:12:21 -0400

    Cleaned up the multi-adder

--- homomorphic_encryption.py
+++ homomorphic_encryption.py
@@ -33,7 +33,6 @@ class Ciphertext():
     aux: list # [int]
     out: int
     modulus: int
-    depth: int # Counter to track multiplicative depth, for debug purposes
 
     # Add together two ciphertexts into one, linearly combining the aux and out.
     # Note that this combines the magnitudes of the error, so if you add waaaaay
@@ -49,20 +48,19 @@ class Ciphertext():
             aux = [(x+y) % self.modulus for x,y in zip(self.aux, other.aux)],
             out = (self.out + other.out) % self.modulus,
             modulus = self.modulus,
-            depth = max(self.depth, other.depth)
         )
 
     # Convert 0 to 1 or 1 to 0
     def flip(self):
-        return Ciphertext(aux=self.aux, out=(self.out+1) % self.modulus, modulus=self.modulus, depth=self.depth)
+        return Ciphertext(aux=self.aux, out=(self.out+1) % self.modulus, modulus=self.modulus)
 
 # A dummy ciphertext to represent zero
-ZERO_CT = Ciphertext(aux=[], out=0, modulus=0, depth=1)
+ZERO_CT = Ciphertext(aux=[], out=0, modulus=0)
 
 # Encrypt a value (remember: out = aux . key + m + 2e)
 def encrypt(key, message, q):
     aux = random_vector(len(key), q)
-    return Ciphertext(aux, (prod(aux, key, q) + noise() + message) % q, q, 1)
+    return Ciphertext(aux, (prod(aux, key, q) + noise() + message) % q, q)
 
 # Partially decrypt a ciphertext, providing the output (0 or 1) plus noise (2e)
 # Useful mainly for debugging
@@ -73,6 +71,11 @@ def partial_decrypt(key, ciphertext):
     if ciphertext.modulus == 0:
         return ciphertext.out
     return (ciphertext.out - prod(ciphertext.aux, key, ciphertext.modulus)) % ciphertext.modulus
+
+# Another debugging method: get magnitude of error
+def error_digits(key, ciphertext):
+    o = partial_decrypt(key, ciphertext)
+    return min(len(str(o)), len(str(ciphertext.modulus-o)))
 
 # Decrypt a ciphertext, outputting the message 0 or 1
 def decrypt(key, ciphertext):
@@ -142,7 +145,6 @@ def multiply_ciphertexts(c1, c2, transit_key):
     comb_ciphertext = sum(b1a2_b2a1_s + a1a2_ss, ZERO_CT)
     # Add b1*b2
     comb_ciphertext.out = (comb_ciphertext.out + b1 * b2) % modulus
-    comb_ciphertext.depth = max(c1.depth, c2.depth) + 1
     return comb_ciphertext
 
 # "Rescale" a value mod q into a value mod p, in a way that preserves evenness of noise
@@ -197,100 +199,48 @@ _and = multiply_ciphertexts
 
 # Kogge-Stone adder, see https://upload.wikimedia.org/wikipedia/commons/1/1c/4_bit_Kogge_Stone_Adder_Example_new.png
 def encoded_add(a, b, tk):
-    origp = [ai + bi for ai, bi in zip(a,b)]
-    p = origp[::]
-    g = [_and(ai, bi, tk) for ai, bi in zip(a,b)]
+    return kogge_stone_propagate(
+        [ai + bi for ai, bi in zip(a,b)],
+        [_and(ai, bi, tk) for ai, bi in zip(a,b)],
+        tk
+    )
+
+def encoded_add3(a, b, c, tk):
+    return kogge_stone_propagate(
+        [ai + bi + ci for ai, bi, ci in zip(a,b,c)],
+        [_and(ai, bi, tk) + _and(ai, ci, tk) + _and(bi, ci, tk) for ai, bi, ci in zip(a,b,c)],
+        tk
+    )
+
+def kogge_stone_propagate(p, g, tk):
+    origp = p[::]
     offset = 1
-    while 2**offset <= len(a):
-        for i in range(0, len(a)-offset):
+    while 2**offset <= len(p):
+        for i in range(0, len(p)-offset):
             g[i+offset] = _or(g[i+offset], _and(p[i+offset], g[i], tk), tk)
             p[i+offset] = _and(p[i], p[i+offset], tk)
         offset *= 2
-    return [origp[0]] + [origp[i] + g[i-1] for i in range(1, len(a))] + [g[-1]]
+    return [origp[0]] + [origp[i] + g[i-1] for i in range(1, len(p))] + [g[-1]]
 
-# Lower-depth adder for k>2 values. For log(p) bit values, has depth log(k)+log(log(k))*log(p)
+# Converts a+b+c into v+w such that a+b+c = v+w. Multiplicative depth 1.
+def three_to_two(a, b, c, zero, tk):
+    return (
+        [ai + bi + ci for ai, bi, ci in zip(a,b,c)] + [zero],
+        [zero] + [_and(ai, bi, tk) + _and(ai, ci, tk) + _and(bi, ci, tk) for ai, bi, ci in zip(a,b,c)]
+    )
+
 def multi_add(values, zero, tk):
-    # Step 1: split out each digit, eg. for the ith digit v1[i], v2[i] ... vk[i]
-    # and compute the log(k) bit sized sum(v1[i] + ...  vk[i])
-    # Depth: log(k)
-    bit_collections = [[[v[i]] for v in values] for i in range(len(values[0]))]
-    for i in range(len(bit_collections)):
-        print("Processing the %d'th collection" % i)
-        b = bit_collections[i]
-        while len(b) > 1:
-            print(len(b))
-            if len(b) % 2:
-                b.append([zero] * len(b[-1]))
-            b = [encoded_add(b[j], b[j+1], tk) for j in range(0, len(b), 2)]
-        # Output the sums
-        bit_collections[i] = b[0]
-    # Perform the rearranging procedure again, converting log(p) log(k)-bit numbers with offsets
-    # into log(k) log(p)-bit numbers
-    summands = [[zero]*i+[b[i] for b in bit_collections]+[zero]*(len(bit_collections[0])-1-i) for i in range(len(bit_collections[0]))]
-    # Add together the log(k) values of size p each. Complexity log(log(k))*log(p)
-    print("Combining outputs")
-    while len(summands) > 1:
-        print(len(summands))
-        if len(summands) % 2:
-            summands.append([zero] * len(summands[-1]))
-        summands = [encoded_add(summands[j], summands[j+1], tk) for j in range(0, len(summands), 2)]
-    return summands[0]
+    while len(values) > 2:
+        o = []
+        for i in range(0, len(values)-2, 3):
+            x, y = three_to_two(values[i], values[i+1], values[i+2], zero, tk)
+            o.extend([x, y])
+        o.extend(values[len(values) - len(values) % 3:])
+        values = o
+    return encoded_add(values[0], values[1], tk) if len(values) == 2 else encoded_add3(values[0], values[1], values[2], tk)
 
 def next_prime(n):
     x = n + (n % 2) + 1
     while pow(2, x, x) != 2:
         x += 2
     return x
-
-short_modulus = 997
-medium_modulus = 99991
-huge_modulus = next_prime(10**150)
-
-def test_add(x, y):
-    S = random_vector(5, huge_modulus)
-    zero = encrypt(S, 0, huge_modulus)
-    one = encrypt(S, 1, huge_modulus)
-    encx = binary_encode(x, 8, zero, one)
-    ency = binary_encode(y, 8, zero, one)
-    encz = encoded_add(encx, ency, mk_transit_key(S, S, huge_modulus))
-    assert sum([decrypt(S, x) << i for i, x in enumerate(encz[:8])]) == x + y
-
-def test():
-    s = random_vector(5, medium_modulus)
-    t = random_vector(5, medium_modulus)
-    tk = mk_transit_key(s, t, medium_modulus)
-    c1 = encrypt(s, 0, medium_modulus)
-    c2 = encrypt(s, 0, medium_modulus)
-    c3 = encrypt(s, 1, medium_modulus)
-    c4 = encrypt(s, 1, medium_modulus)
-    assert decrypt(s, c1) == decrypt(s, c2) == 0
-    assert decrypt(s, c3) == decrypt(s, c4) == 1
-    assert decrypt(s, c1 + c2) == 0
-    assert decrypt(s, c1 + c4) == 1
-    assert decrypt(s, c3 + c4) == 0
-    assert decrypt(t, multiply_ciphertexts(c1, c2, tk)) == 0
-    assert decrypt(t, multiply_ciphertexts(c1, c4, tk)) == 0
-    assert decrypt(t, multiply_ciphertexts(c3, c4, tk)) == 1
-    u = random_vector(3, short_modulus)
-    uk = mk_modulus_change_key(s, u, medium_modulus, short_modulus)
-    for c in (c1, c2, c3, c4):
-        assert decrypt(u, change_ciphertext_modulus(c, uk)) == decrypt(s, c)
-    for cl in (c1, c2, c3, c4):
-        for cr in (c1, c2, c3, c4):
-            cm = multiply_ciphertexts(cl, cr, tk)
-            tuk = mk_modulus_change_key(t, u, medium_modulus, short_modulus)
-            assert decrypt(u, change_ciphertext_modulus(cm, tuk)) == decrypt(t, cm) == decrypt(s, cl) * decrypt(s, cr)
-    S = random_vector(5, huge_modulus)
-    zero = encrypt(S, 0, huge_modulus)
-    one = encrypt(S, 1, huge_modulus)
-    forty_two = binary_encode(42, 8, zero, one)
-    sixty_nine = binary_encode(69, 8, zero, one)
-    one_one_one = encoded_add(forty_two, sixty_nine, mk_transit_key(S, S, huge_modulus))
-    assert binary_decrypt(S, one_one_one) == 111
-    two, three, four, five = (binary_encode(i, 4, zero, one) for i in (2, 3, 4, 5))
-    fourteen = multi_add([two, three, four, five], zero, mk_transit_key(S, S, huge_modulus))
-    assert binary_decrypt(S, fourteen[:4]) == 14
-    print("Tests passed")
-    
-if __name__ == '__main__':
-    test()
