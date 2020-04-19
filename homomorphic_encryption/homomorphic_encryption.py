78b7684cbf2fe09006b291565ea03aa5a6735952
Vitalik Buterin <v@buterin.com>
2020-04-18 21:38:59 -0400

    Switched to https://eprint.iacr.org/2012/078.pdf (still on progress)

--- homomorphic_encryption/homomorphic_encryption.py
+++ homomorphic_encryption/homomorphic_encryption.py
@@ -6,6 +6,8 @@
 from dataclasses import dataclass
 import random
 
+ERROR_DIGITS = 4
+
 # Returns the bit length of n, eg. 6 -> 110 -> 3, 31 -> 11111 -> 5, 32 -> 100000 -> 6
 def bit_length(n):
     return len(bin(n))-2
@@ -14,24 +16,25 @@ def bit_length(n):
 def random_vector(l, modulus):
     return [random.randrange(modulus) for _ in range(l)]
 
-mk_key = random_vector
-
 # Generates an (even) noise value within some small range
 def noise():
-    return 2 * random.randrange(-3, 4)
+    return random.randrange(-2**ERROR_DIGITS, ERROR_DIGITS)
 
 # Inner product of two vectors, that is compute sum(vec1[i] * vec2[i]).
 # Allows special case for empty vectors
 def prod(vec1, vec2, modulus):
     return sum([i*j for i,j in zip(vec1, vec2)]) % modulus if (vec1 and vec2) else 0
 
+# Generates a private key
+def generate_key(length, modulus):
+    return [1] + [(2**ERROR_DIGITS + noise()) % modulus for _ in range(1, length)]
+
 # A ciphertext representing some value m in {0,1}
 # Ciphertexts are of the form (aux, out) where out = aux . key + m + 2e
 # where . is the inner product and 2e is an even error term
 @dataclass
 class Ciphertext():
-    aux: list # [int]
-    out: int
+    values: list # [int]
     modulus: int
 
     # Add together two ciphertexts into one, linearly combining the aux and out.
@@ -43,36 +46,58 @@ class Ciphertext():
             return other
         if other.modulus == 0:
             return self
-        assert self.modulus == other.modulus and len(self.aux) == len(other.aux)
+        assert self.modulus == other.modulus and len(self.values) == len(other.values)
         return Ciphertext(
-            aux = [(x+y) % self.modulus for x,y in zip(self.aux, other.aux)],
-            out = (self.out + other.out) % self.modulus,
+            values = [(x+y) % self.modulus for x,y in zip(self.values, other.values)],
             modulus = self.modulus,
         )
 
     # Convert 0 to 1 or 1 to 0
     def flip(self):
-        return Ciphertext(aux=self.aux, out=(self.out+1) % self.modulus, modulus=self.modulus)
+        new_first_value = (self.values[0] + self.modulus // 2) % self.modulus
+        return Ciphertext(values=new_first_value+self.values[1:], modulus=self.modulus)
 
 # A dummy ciphertext to represent zero
-ZERO_CT = Ciphertext(aux=[], out=0, modulus=0)
+ZERO_CT = Ciphertext(values=[], modulus=0)
 
 def sum_ciphertexts(ciphertexts):
-    L, m = len(ciphertexts[0].aux), ciphertexts[0].modulus
-    if len(ciphertexts) == 0:
-        return Ciphertext(aux=[], out=0, modulus=0)
+    assert len(ciphertexts) >= 1
+    L, m = len(ciphertexts[0].values), ciphertexts[0].modulus
     for c in ciphertexts[1:]:
-        assert c.modulus == m and len(c.aux) == L
+        assert c.modulus == m and len(c.values) == L
     return Ciphertext(
-        aux = [sum([c.aux[i] for c in ciphertexts]) % m for i in range(L)],
-        out = sum(c.out for c in ciphertexts) % m,
+        values = [sum([c.values[i] for c in ciphertexts]) % m for i in range(L)],
         modulus = m
     )
 
+
+# Generate public key (bunch of encryptions of zero)
+@dataclass
+class PublicKey():
+    samples: list #[Ciphertext]
+
+def mk_public_key(key, q):
+    o = []
+    for i in range(100):
+        rv = random_vector(len(key)-1, q)
+        canceling_value = -prod(rv, key[1:], q) % q
+        o.append(Ciphertext([(noise() + canceling_value) % q] + rv, q))
+    return PublicKey(samples=o)
+
 # Encrypt a value (remember: out = aux . key + m + 2e)
 def encrypt(key, message, q):
-    aux = random_vector(len(key), q)
-    return Ciphertext(aux, (prod(aux, key, q) + noise() + message) % q, q)
+    return partial_encrypt(key, message * q // 2, q)
+
+# Partially encrypt a value, assuming it's already in rescaled form
+# (ie. 0 for a normal 0 and q//2 for a normal 1). Used for encrypting
+# key shares for relinerization
+def partial_encrypt(key, message, q):
+    assert key[0] == 1
+    for k in key:
+        assert k <= 2**(ERROR_DIGITS+1)
+    rv = random_vector(len(key)-1, q)
+    canceling_value = -prod(rv, key[1:], q) % q
+    return Ciphertext([(noise() + canceling_value + message) % q] + rv, q)
 
 # Partially decrypt a ciphertext, providing the output (0 or 1) plus noise (2e)
 # Useful mainly for debugging
@@ -80,54 +105,34 @@ def encrypt(key, message, q):
 # you get a negative value, which appears to be a value close to the modulus
 # with a flipped parity, eg. if modulus = 97, -4 appears as 93
 def partial_decrypt(key, ciphertext):
-    if ciphertext.modulus == 0:
-        return ciphertext.out
-    return (ciphertext.out - prod(ciphertext.aux, key, ciphertext.modulus)) % ciphertext.modulus
+    return prod(ciphertext.values, key, ciphertext.modulus)
 
 # Another debugging method: get magnitude of error
 def error_digits(key, ciphertext):
     o = partial_decrypt(key, ciphertext)
-    return min(len(str(o)), len(str(ciphertext.modulus-o)))
+    return len(str(min(
+        o,
+        ciphertext.modulus-o,
+        abs(ciphertext.modulus//2-o)
+    )))
 
 # Decrypt a ciphertext, outputting the message 0 or 1
 def decrypt(key, ciphertext):
-    # Special case for empty aux
-    if ciphertext.modulus == 0:
-        return ciphertext.out % 2
-    aux, out, q = ciphertext.aux, ciphertext.out, ciphertext.modulus
-    half_q = (q // 2) - (q // 2) % 2
-    return ((out - prod(aux, key, q) + half_q) % q) % 2
-
-# Expresses a number in a given base
-def express_in_base(n, base, length):
-    # Optimized special case for base 16
-    if BASE == 16:
-        o = []
-        for byte in n.to_bytes(length//2+1, 'little'): 
-            o.append(byte%16)
-            o.append(byte//16)
-        return o[:length]
-    else:
-        o = []
-        while n > 0:
-            o.append(n % base)
-            n //= base
-        return o + [0] * (length - len(o))
-
-BITS_PER_DIGIT = 4
-BASE = 2**BITS_PER_DIGIT
+    values, q = ciphertext.values, ciphertext.modulus
+    return 1 if (q//4 < prod(values, key, q) <= (q*3)//4) else 0
 
 @dataclass
 class TransitKeyComponent():
     digits: list # [[ciphertext]]
 
     def get_combination_for(self, index):
-        representation = express_in_base(index, BASE, len(self.digits))
-        return sum_ciphertexts([self.digits[power][representation[power]-1] for power in range(len(self.digits)) if representation[power] > 0])
+        if index:
+            return sum_ciphertexts([self.digits[power] for power in range(len(self.digits)) if (index >> power) % 2 == 1])
+        else:
+            return Ciphertext(values=[0] * len(self.digits[0].values), modulus=self.digits[0].modulus)
 
 @dataclass
 class TransitKey():
-    singles: list # [TransitKeyComponent]
     pairs: list # [list][TransitKeyComponent]
 
 @dataclass
@@ -136,33 +141,25 @@ class ModulusChangeKey():
     old_modulus: int
     new_modulus: int
 
+def flatten_key(s, modulus):
+    return sum([[(x >> i) % 2 for i in range(ERROR_DIGITS+1)] for x in s], [])
+
+def flatten_ciphertext(ct):
+    return Ciphertext(
+        values=sum([[(x << i) % ct.modulus for i in range(ERROR_DIGITS+1)] for x in ct.values], []),
+        modulus=ct.modulus
+    )
+
 def mk_transit_key(s, t, q):
     # For each v=s[i], and for each product v = s[i] * s[j], encrypt v, v*2, v*3... v*base, v*2*base, v*3*base...
     # under the key `t`.
     # This allows us to compute s[i] * b or s[i] * s[j] * b for any b with a logarithmic number
     # of additions (and hence logarithmic-sized error blowup)
-
-    # Determine the highest power we need to compute up to
-    power_count = 0
-    while BASE**power_count < q:
-        power_count += 1
-
-    # Precompute s[i]*s[j] products
-    s_products = [[s[i] * s[j] % q if j>=i else None for j in range(len(s))] for i in range(len(s))]
-
-    return TransitKey(
-        singles = [
-            TransitKeyComponent(digits=[[encrypt(t, (s[i] * digit) << (BITS_PER_DIGIT * power), q) for digit in range(1, BASE)] for power in range(power_count)])
-            for i in range(len(s))
-        ],
-        pairs = [
-            [
-                TransitKeyComponent(digits=[[encrypt(t, (s_products[min(i,j)][max(i,j)] * digit) << (BITS_PER_DIGIT * power), q) for digit in range(1, BASE)] for power in range(power_count)])
-                for j in range(len(s))
-            ]
-            for i in range(len(s))
-        ],
-    )
+    s = flatten_key(s, q)
+    pairs = []
+    for i in range(len(s)):
+        pairs.append([TransitKeyComponent(digits=[partial_encrypt(t, ((s[i]*s[j])<<power) % q, q) for power in range(bit_length(q))]) for j in range(i+1)])
+    return TransitKey(pairs=pairs)
 
 def multiply_ciphertexts(c1, c2, transit_key):
     # The idea here is that we take the equation
@@ -174,60 +171,10 @@ def multiply_ciphertexts(c1, c2, transit_key):
     # except instead of evaluating it directly (we can't because we don't have the key), we
     # use the transit key to evaluate the equation as a linear combination of s[i] and s[i]*s[j],
     # giving us the decrypted output, encrypted under `t` (the target key of the transit key)
-    a1, b1 = c1.aux, c1.out
-    a2, b2 = c2.aux, c2.out
-    assert c1.modulus == c2.modulus and len(a1) == len(a2) == len(transit_key.singles)
-    dim, modulus = len(a1), c1.modulus
-    # All monomials in -(b1*a2).s - (b2*a1).s
-    b1a2_b2a1_s = [transit_key.singles[i].get_combination_for((-b1 * a2[i] - b2 * a1[i]) % modulus) for i in range(dim)]
-    # All monomials in s1⁰s2.a1⁰a2
-    a1a2_ss = [transit_key.pairs[i][j].get_combination_for(a1[i] * a2[j] % modulus) for i in range(dim) for j in range(dim)]
-    # Combined ciphertext
-    comb_ciphertext = sum(b1a2_b2a1_s + a1a2_ss, ZERO_CT)
-    # Add b1*b2
-    comb_ciphertext.out = (comb_ciphertext.out + b1 * b2) % modulus
-    return comb_ciphertext
-
-# "Rescale" a value mod q into a value mod p, in a way that preserves evenness of noise
-# This works by first halving x (note that "even" here means {-2e: -q/4 < e < q/4}), getting
-# us to an error of {-q/4 < e < q/4}. Multiplying by p/q gets us to {-p/4 < e' < p/4}, with
-# up to 1/2 extra error from imperfect division. Then we multiply back by two.
-# Note that the goal here is not to preserve evenness of x itself, the goal is that for any x,y
-# we want to preserve the evenness of x-y
-#
-# This method is used to convert a high-modulus ciphertext into a low-modulus ciphertext, making
-# it easier to decrypt in a circuit for bootstrapping
-def rescale_modulus(x, q, p):
-    half = (q+1)//2
-    half_x = (x*half) % q
-    scaled_half_x = (half_x * p) // q
-    scaled_x = (scaled_half_x * 2) % p
-    return scaled_x
-
-# A transfer key similar to the key above, except with no quadratic terms and with a modulus change
-# Goal is to convert a ciphertext from being encrypted under key s (modulo q) to key t (modulo p)
-def mk_modulus_change_key(s, t, q, p):
-    power_count = 0
-    while BASE**power_count < q:
-        power_count += 1
-    return ModulusChangeKey(
-        items = [
-            TransitKeyComponent(digits=[[encrypt(t, rescale_modulus(s[i] * digit * BASE**power, q, p), p) for digit in range(1, BASE)] for power in range(power_count)])
-            for i in range(len(s))
-        ],
-        old_modulus=q,
-        new_modulus=p,
-    )
-
-# Change a ciphertext using a given modulus change key
-def change_ciphertext_modulus(ct, modulus_change_key):
-    oldq, newq = modulus_change_key.old_modulus, modulus_change_key.new_modulus
-    new_ct = sum([
-        modulus_change_key.items[i].get_combination_for(oldq - ct.aux[i])
-        for i in range(len(ct.aux))
-    ], ZERO_CT)
-    new_ct.out = (new_ct.out + rescale_modulus(ct.out, oldq, newq)) % newq
-    return new_ct
+    c1, c2 = flatten_ciphertext(c1), flatten_ciphertext(c2)
+    v1, v2 = c1.values, c2.values
+    dim, modulus = len(v1), c1.modulus
+    return sum_ciphertexts([transit_key.pairs[max(i,j)][min(i,j)].get_combination_for((v1[i] * v2[j] * 2 // modulus) % modulus) for i in range(dim) for j in range(dim)])
 
 def binary_encode(integer, length, encoded_zero, encoded_one):
     return [encoded_one if integer & (1 << i) else encoded_zero for i in range(length)]
@@ -259,9 +206,12 @@ def kogge_stone_propagate(p, g, tk):
     origp = p[::]
     offset = 1
     while 2**offset <= len(p):
+        newg = g[::]
+        newp = p[::]
         for i in range(0, len(p)-offset):
-            g[i+offset] = _or(g[i+offset], _and(p[i+offset], g[i], tk), tk)
-            p[i+offset] = _and(p[i], p[i+offset], tk)
+            newg[i+offset] = _or(g[i+offset], _and(p[i+offset], g[i], tk), tk)
+            newp[i+offset] = _and(p[i], p[i+offset], tk)
+        g, p = newg, newp
         offset *= 2
     return [origp[0]] + [origp[i] + g[i-1] for i in range(1, len(p))] + [g[-1]]
 
--- homomorphic_encryption/tests.py
+++ homomorphic_encryption/tests.py
@@ -1,18 +1,17 @@
 from homomorphic_encryption import next_prime, random_vector, encrypt, decrypt, \
     binary_encode, encoded_add, mk_transit_key, error_digits, binary_decrypt, \
-    change_ciphertext_modulus, mk_modulus_change_key, multiply_ciphertexts, \
+    multiply_ciphertexts, generate_key, bit_length, \
     three_to_two, encoded_add3, multi_add, add_and_return_remainder_parity_and_rangecheck, \
-    partial_decrypt, mk_bootstrapping_key, bootstrap
+    partial_decrypt, mk_bootstrapping_key, bootstrap, flatten_ciphertext, flatten_key
 import random
 
 short_modulus = 167
-medium_modulus = 49999
-large_modulus = 10**100 + 267
-very_large_modulus = 10**320 + 699
-huge_modulus = 10**2000 + 4561
+medium_modulus = 10**8
+large_modulus = 10**30 + 57
+huge_modulus = 10**100 + 267
 
 def generate_keys(modulus):
-    s = random_vector(5, modulus)
+    s = generate_key(6, modulus)
     zero = encrypt(s, 0, modulus)
     one = encrypt(s, 1, modulus)
     tk = mk_transit_key(s, s, modulus)
@@ -21,11 +20,12 @@ def generate_keys(modulus):
 def test_add(x, y, keys):
     print("Testing addition circuit for {} and {}".format(x,y))
     S, zero, one, tk = keys
-    encx = binary_encode(x, 8, zero, one)
-    ency = binary_encode(y, 8, zero, one)
+    bitcount = bit_length(x+y)
+    encx = binary_encode(x, bitcount, zero, one)
+    ency = binary_encode(y, bitcount, zero, one)
     encz = encoded_add(encx, ency, tk)
     print('error digits', [error_digits(S, x) for x in encz])
-    assert binary_decrypt(S, encz[:8]) == x + y
+    assert binary_decrypt(S, encz[:bitcount]) == x + y
 
 def test_add3(x, y, z, keys):
     print("Testing addition circuit for {} {} {}".format(x,y, z))
@@ -52,7 +52,7 @@ def test_multiadd(values, keys):
     assert sum(values) < 256
     encoded = [binary_encode(v, 8, zero, one) for v in values]
     z = multi_add(encoded, zero, tk)
-    print([error_digits(S, x) for x in z])
+    print('error digits', [error_digits(S, x) for x in z])
     assert binary_decrypt(S, z[:8]) == sum(values)
 
 def test_add_and_return_remainder_parity_and_rangecheck(values, start, end, keys):
@@ -64,60 +64,62 @@ def test_add_and_return_remainder_parity_and_rangecheck(values, start, end, keys
     assert decrypt(S, parity) == (sum(values)-start) % 2
 
 def test():
-    s = random_vector(5, medium_modulus)
-    t = random_vector(5, medium_modulus)
-    tk = mk_transit_key(s, t, medium_modulus)
+    s = generate_key(6, medium_modulus)
+    sk = mk_transit_key(s, s, medium_modulus)
     c1 = encrypt(s, 0, medium_modulus)
     c2 = encrypt(s, 0, medium_modulus)
     c3 = encrypt(s, 1, medium_modulus)
     c4 = encrypt(s, 1, medium_modulus)
     assert decrypt(s, c1) == decrypt(s, c2) == 0
     assert decrypt(s, c3) == decrypt(s, c4) == 1
+    assert decrypt(flatten_key(s, medium_modulus), flatten_ciphertext(c1)) == 0
+    assert decrypt(flatten_key(s, medium_modulus), flatten_ciphertext(c2)) == 0
+    assert decrypt(flatten_key(s, medium_modulus), flatten_ciphertext(c3)) == 1
+    assert decrypt(flatten_key(s, medium_modulus), flatten_ciphertext(c4)) == 1
     assert decrypt(s, c1 + c2) == 0
     assert decrypt(s, c1 + c4) == 1
     assert decrypt(s, c3 + c4) == 0
-    assert decrypt(t, multiply_ciphertexts(c1, c2, tk)) == 0
-    assert decrypt(t, multiply_ciphertexts(c1, c4, tk)) == 0
-    assert decrypt(t, multiply_ciphertexts(c3, c4, tk)) == 1
+    assert decrypt(s, c1 + c3) == 1
+    assert decrypt(s, c4 + c4) == 0
+    assert decrypt(s, multiply_ciphertexts(c1, c2, sk)) == 0
+    assert decrypt(s, multiply_ciphertexts(c1, c3, sk)) == 0
+    assert decrypt(s, multiply_ciphertexts(c1, c4, sk)) == 0
+    assert decrypt(s, multiply_ciphertexts(c3, c3, sk)) == 1
+    assert decrypt(s, multiply_ciphertexts(c3, c4, sk)) == 1
+    print('ed', error_digits(s, multiply_ciphertexts(c3, c4, sk)))
     print("Basic tests passed")
-    u = random_vector(3, short_modulus)
-    uk = mk_modulus_change_key(s, u, medium_modulus, short_modulus)
-    for c in (c1, c2, c3, c4):
-        assert decrypt(u, change_ciphertext_modulus(c, uk)) == decrypt(s, c)
-    for cl in (c1, c2, c3, c4):
-        for cr in (c1, c2, c3, c4):
-            cm = multiply_ciphertexts(cl, cr, tk)
-            tuk = mk_modulus_change_key(t, u, medium_modulus, short_modulus)
-            assert decrypt(u, change_ciphertext_modulus(cm, tuk)) == decrypt(t, cm) == decrypt(s, cl) * decrypt(s, cr)
-    print("Modulus change tests passed")
+    x = encrypt(s, 0, medium_modulus)
+    for i in range(5):
+        print(i, error_digits(s, x), partial_decrypt(s, x))
+        x = multiply_ciphertexts(x, x, sk)
     keys = {
         short_modulus: generate_keys(short_modulus),
         medium_modulus: generate_keys(medium_modulus),
         large_modulus: generate_keys(large_modulus),
-        very_large_modulus: generate_keys(very_large_modulus),
         huge_modulus: generate_keys(huge_modulus),
     }
     print("Generated keys")
     test_add(42, 69, keys[large_modulus])
+    test_add(123456789, 987654321, keys[huge_modulus])
     test_add3(13, 37, 42, keys[large_modulus])
     test_three_to_two(random.randrange(85), random.randrange(85), random.randrange(85), keys[large_modulus])
-    test_multiadd([random.randrange(32) for _ in range(8)], keys[huge_modulus])
+    test_multiadd([random.randrange(32) for _ in range(8)], keys[large_modulus])
     print("Multiadd tests passed")
     print("Testing add/parity/rangecheck")
     print("42 vs [40, 50]")
-    test_add_and_return_remainder_parity_and_rangecheck([7, 11, 24], 40, 50, keys[very_large_modulus])
+    test_add_and_return_remainder_parity_and_rangecheck([7, 11, 24], 40, 50, keys[large_modulus])
     print("42 vs [41, 50]")
-    test_add_and_return_remainder_parity_and_rangecheck([7, 11, 24], 41, 50, keys[very_large_modulus])
+    test_add_and_return_remainder_parity_and_rangecheck([7, 11, 24], 41, 50, keys[large_modulus])
     print("42 vs [42, 50]")
-    test_add_and_return_remainder_parity_and_rangecheck([7, 11, 24], 42, 50, keys[very_large_modulus])
+    test_add_and_return_remainder_parity_and_rangecheck([7, 11, 24], 42, 50, keys[large_modulus])
     print("42 vs [43, 50]")
-    test_add_and_return_remainder_parity_and_rangecheck([7, 11, 24], 43, 50, keys[very_large_modulus])
+    test_add_and_return_remainder_parity_and_rangecheck([7, 11, 24], 43, 50, keys[large_modulus])
     print("42 vs [40, 43]")
-    test_add_and_return_remainder_parity_and_rangecheck([7, 11, 24], 40, 43, keys[very_large_modulus])
+    test_add_and_return_remainder_parity_and_rangecheck([7, 11, 24], 40, 43, keys[large_modulus])
     print("42 vs [40, 42]")
-    test_add_and_return_remainder_parity_and_rangecheck([7, 11, 24], 40, 42, keys[very_large_modulus])
+    test_add_and_return_remainder_parity_and_rangecheck([7, 11, 24], 40, 42, keys[large_modulus])
     print("42 vs [40, 41]")
-    test_add_and_return_remainder_parity_and_rangecheck([7, 11, 24], 40, 41, keys[very_large_modulus])
+    test_add_and_return_remainder_parity_and_rangecheck([7, 11, 24], 40, 41, keys[large_modulus])
     print("Add/parity/rangecheck tests passed")
     print("Starting bootstrap test")
     t, bit0, bit1, tk = keys[huge_modulus]
@@ -125,9 +127,9 @@ def test():
     s, smallzero, smallone, _ = keys[short_modulus]
     ENCRYPTING_BIT = 1
     bit = bit1 if ENCRYPTING_BIT == 1 else bit0
-    for i in range(8):
+    for i in range(20):
         bit = multiply_ciphertexts(bit, bit, tk)
-    print(error_digits(t, bit))
+        print(error_digits(t, bit))
     assert decrypt(t, bit) == ENCRYPTING_BIT
     tmk = mk_modulus_change_key(t, m, huge_modulus, medium_modulus)
     mediumbit = change_ciphertext_modulus(bit, tmk)
